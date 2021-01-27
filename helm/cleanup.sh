e09461c94f7c5d4b8f3b2ca51554c833fb879e76
Philippe HUET <phuet@redhat.com>
2021-01-27 17:40:46 +0100

    Do cleanup for main demo

--- helm/cleanup.sh
+++ helm/cleanup.sh
@@ -2,11 +2,10 @@
 
 IMAGE_TAG=$(../tools/image-tag)
 
-NS=meetup-demo-${IMAGE_TAG}
+NS=meetup-demo-02
 NS=$(tr "[A-Z]" "[a-z]" <<<$NS)
 
 oc project ${NS}
 
 helm uninstall vtodos
-oc adm policy remove-scc-from-user privileged -z vtodos-vue-todos
 oc delete project ${NS}
